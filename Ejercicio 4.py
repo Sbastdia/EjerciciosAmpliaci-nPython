2d7e6fa9445e71f4a437d82b7c5462d558216942
Sbastdia <sbastdia@myuax.com>
2021-11-05 16:01:29 +0100

    ejercicio 4

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -8,7 +8,11 @@ import sys
 def staircase(n):
     for i in range(0,n):
         escalera=''
-        escalera+='#\n'+i
+        for p in range(0,n-1-i):
+            escalera=escalera + " "
+        for s in range(0,i+1):
+            escalera=escalera + "# "
+        print(escalera)
 
 
 if __name__ == '__main__':
