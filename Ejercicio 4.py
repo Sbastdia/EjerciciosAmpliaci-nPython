6387b38f565c3a4022b1ecd03d9304a1fb509635
Sbastdia <sbastdia@myuax.com>
2021-11-05 14:04:15 +0100

    ejercicio 4

---
+++ Ejercicio 4.py
@@ -0,0 +1,16 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+def staircase(n):
+    for i in range(0,n):
+        escalera=''
+        escalera+='#\n'+i
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    staircase(n)
